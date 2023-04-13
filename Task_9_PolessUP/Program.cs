3b631624e04c14a4ef1cfa9ebfbae14a73fde1de
CyberSanyok <sashad92175@gmail.com>
2023-04-13 10:33:55 +0300

    Добавьте файлы проекта.

---
+++ Task_9_PolessUP/Program.cs
@@ -0,0 +1,11 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int date = 2000;
+        Console.WriteLine(date+": "+DateTime.IsLeapYear(date));
+        date = 2015;
+        Console.WriteLine(date+": "+DateTime.IsLeapYear(date));
+
+    }
+}
